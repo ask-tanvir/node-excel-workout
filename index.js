fb7da4264d464a5c911a6ac74298901c8be82876
Tanvir Islam Streame <engr.tanvir.islam.1@gmail.com>
2023-05-12 13:06:59 +0600

    Add sum of mark column

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const ExcelJS = require('exceljs');
 const workbook = new ExcelJS.Workbook();
 const sheet = workbook.addWorksheet('My Sheet');
-sheet.columns = [
-  { header: 'Id', key: 'id', width: 10 },
-  { header: 'Name', key: 'name', width: 32 },
-  { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
-];
-
-sheet.addRows([
-  {
-    id: 1, name: 'Tanvir', DOB: new Date()
-  },
-  {
-    id: 2, name: 'Biplab', DOB: new Date()
-  }
-]);
+sheet.addTable({
+  name: 'MyTable',
+  ref: 'A1',
+  headerRow: true,
+  totalsRow: true,
+  columns: [
+    {name: 'Subject', totalsRowLabel: 'Sum:'},
+    {name: 'Mark', totalsRowFunction: 'sum', filterButton: true},
+  ],
+  rows: [
+    ["English", 70.10],
+    ["Bangla", 70],
+    ["Math", 90],
+		["Science", 85],
+  ]
+});
 
 workbook.xlsx.writeFile('data.xlsx');
